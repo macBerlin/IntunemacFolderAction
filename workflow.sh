38a8d58722f4e504aa0dfcd58e8c75823dc97362
macBerlin <75625721+macBerlin@users.noreply.github.com>
2021-11-13 11:26:15 +0100

    Update workflow.sh

--- workflow.sh
+++ workflow.sh
@@ -1,30 +1,36 @@
-FILETYPEDIAG=$(/usr/bin/file $1)
+# requirements: https://github.com/msintuneappsdk/intune-app-wrapping-tool-mac
+# Folder Action for macOS to wrap pkg to intunemac automaticly.
+# Michael Rieder 2021 
+
+# Get the directory path from filename
+DIR="$(dirname "${1}")" ;
+mkdir -p "${DIR}/intunemac"
+mkdir -p "${DIR}/source_packages"
+mkdir -p "${DIR}/source_failed"
 
 
-## We run only if the the file contains a xar archive
+OIFS="$IFS"
+IFS=$'\n'
+for PKGFILES in $(ls  ${DIR}/*.pkg)
+	do
+	echo "Convert file: ${PKGFILES}" >>/tmp/IntuneAppUtil.log
+	FILETYPEDIAG=$(/usr/bin/file $PKGFILES)
+	if [[ $FILETYPEDIAG == *"xar archive"* ]]; then
+		echo "Start wrapper for package.."  >>/tmp/IntuneAppUtil.log
 
-if [[ $FILETYPEDIAG == *"xar archive"* ]]; then
-	
-	# Get the directory path from filename
-	DIR="$(dirname "${1}")" ;
-	
-	# Start Wrapping tool and save output in the same directory as the source.
-	/usr/local/bin/IntuneAppUtil -c "${1}"  -o "${DIR}" -v >>/tmp/IntuneAppUtil.log
-	
-	# If the exit code is equal 0 the process is complete. 
-	if [[ $? -eq 0 ]]; then
-		mv "${1}" "${DIR}/Archive/"
-	else
-		echo "***** ERRROR on processing file $1" >>/tmp/IntuneAppUtil.log
+		# Start Wrapping tool and save output in the same directory as the source.
+		/usr/local/bin/IntuneAppUtil -c "${PKGFILES}" -o "${DIR}/intunemac/" -v >>/tmp/IntuneAppUtil.log
+		if [[ $? -eq 0 ]]; then
+			mv "${PKGFILES}" "${DIR}/source_packages/"
+		else
+			echo "***** ERRROR on processing file $1" >>/tmp/IntuneAppUtil.log
+			mv "${PKGFILES}" "${DIR}/source_failed/"
+		fi
+
+	else 
+		echo "***** ERRROR unknown package format $PKGFILES" >>/tmp/IntuneAppUtil.log
 	fi
 
-else 
-	echo "***** ERRROR unknown package format $1" >>/tmp/IntuneAppUtil.log
-fi
-
-
-
-
-
-
+done
+IFS="$OIFS"
 
